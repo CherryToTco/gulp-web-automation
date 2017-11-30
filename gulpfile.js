daa63703ec92d8405fdda0fd48963829ec787c80
why <isWhyoo@163.com>
2017-11-30 10:55:41 +0800

    优化

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,6 @@ gulp.task('fileinclude', function() {
     }))
     .pipe(plugins.minifyHtml())
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream())
     .pipe(browserSync.reload({stream:true}))
 });
 
@@ -30,7 +29,6 @@ gulp.task('scss',function(){
       cascade: false
     }))
     .pipe(plugins.minifyCss())
-    .pipe(browserSync.stream())
     .pipe(browserSync.reload({stream:true}))
     .pipe(gulp.dest('dist'))
 })
@@ -49,24 +47,38 @@ gulp.task('webpack',function(callback){
   })
 })
 
+gulp.task('minify-img',function () {
+  return gulp.src('./src/static/images/*.{png,jpg,jpeg,gif,ico}',{base:'src'})
+    .pipe(plugins.imagemin({
+      optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+      progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+      interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+      multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+    }))
+    .pipe(plugins.rename(function (path) {
+      path.basename += '.min'
+    }))
+    .pipe(gulp.dest('dist'))
+})
+
 gulp.task('js',['webpack'],function () {
   return gulp.src('./src/views/**/bundle.js')
     .pipe(plugins.uglify())
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream())
     .pipe(browserSync.reload({stream:true}))
 })
 
 gulp.task('copy',function () {
-  return gulp.src('./src/static/**',{base:'src'})
+  return gulp.src(['./src/static/**','!./src/static/images/**'],{base:'src'})
     .pipe(gulp.dest('dist'))
 })
 
 gulp.task('watch',function () {
   gulp.watch('./src/**/*.html', ['fileinclude']);
   gulp.watch('./src/**/*.scss', ['scss']);
+  gulp.watch('./src/static/images/**', ['minify-img']);
   gulp.watch('./src/views/**/*.js', ['js']);
-  console.log('html,js,scss发生了改动');
+  console.log('html,js,scss,img发生了改动');
 })
 
 gulp.task('server', function(){
@@ -86,6 +98,7 @@ gulp.task('prod',['clean'],function () {
   gulp.start('fileinclude');
   gulp.start('scss');
   gulp.start('copy');
+  gulp.start('minify-img');
   gulp.start('js');
 })
 
